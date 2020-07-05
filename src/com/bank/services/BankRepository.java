f3b2dd605b3caaf51a129a48d0c632d5a2cbe310
Suraj Shrestha <shresthasuraj62@gmail.com>
2020-07-05 12:31:43 -0500

    Added the DB connection file to ignore

--- src/com/bank/services/BankRepository.java
+++ src/com/bank/services/BankRepository.java
@@ -1,5 +1,51 @@
 package com.bank.services;
+import com.bank.domain.*;
+
+import java.util.List;
 
 public class BankRepository {
+    public Result add(Patron patron){
+        return Result.SUCCESS;
+    }
+    public Result remove(Patron patron){
+        return Result.SUCCESS;
+    }
+    public Result update(Patron patron){
+        return Result.SUCCESS;
+    }
+    public Patron findPatron(int id){
+    }
+    public List<Patron> findPatron(String name){
+
+    }
+    public Result transact(Transaction transaction){
+        return Result.SUCCESS;
+    }
+    public Transaction findTransaction(int id){
+
+    }
+    public Result add(Bank bank){
+        return Result.SUCCESS;
+    }
+    public Result remove(Bank bank){
+        return Result.SUCCESS;
+    }
+    public Result update(Bank bank){
+        return Result.SUCCESS;
+    }
+    public Bank findBank(int id){
+
+    }
+    public List<Bank> findBank(String name){
+
+    }
+    public Result add(Accout accout){
+        return Result.SUCCESS;
+    }
+    public Result update(Account account){
+        return Result.SUCCESS;
+    }
+
+
 
 }
